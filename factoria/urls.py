0c3729668da6a02d9a154cea1abd6693dd99285e
Dan <dan.timefly@gmail.com>
2022-09-24 17:22:44 +0200

    Added frontend template and static files

--- factoria/urls.py
+++ factoria/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from website.views import homepage_view, products_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', homepage_view, name="homepage"),
+    path('products/', products_view, name="products"),
 ]
