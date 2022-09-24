b13b908bbca97b9a3670059ccf52d575b1b7c735
kamil-korwiel <kamilkorwiel@gmail.com>
2022-09-24 12:18:06 +0200

    Add crud funions and change in settings PROJECT_ROOT

--- factoria/apps/busines/views.py
+++ factoria/apps/busines/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+def create(request:HttpRequest):
+    return HttpResponse("Hello, Form Create")
+
+def retrive(request:HttpRequest):
+    return HttpResponse("Hello, Form Retrive")
+
+def update(request:HttpRequest,id):
+    return HttpResponse(f"Hello, Form Update id={id}")
+    
+def delete(request:HttpRequest,id):
+    return HttpResponse(f"Hello, Form Delete id={id}")
+    
